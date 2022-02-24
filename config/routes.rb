cf787a07a693c7f09a01152188eb1dab1cee88db
saaya0 <sayaaa04155@gmail.com>
2022-02-24 10:39:26 +0000

    add admin device

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  devise_for :admins, controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+  
+  scope module: :admin do
+      resources :admins, only: [:index]
+  end
+  
+  root to: 'homes#top'
+
 end
+
