90d99864f4edc490368882d91eee3e53729ccc79
saaya0 <sayaaa04155@gmail.com>
2022-03-03 05:54:57 +0000

    add header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_header
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
     def after_sign_up_path_for(resource)
         admins_path
@@ -15,7 +17,12 @@ class ApplicationController < ActionController::Base
 
     private
 
-
+    
+    def set_header
+        @header = true
+        @path = [root_path, new_admin_registration_path, new_admin_session_path]
+    end
+    
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana])
     end
